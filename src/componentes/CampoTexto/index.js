c703a4e5b866d7b891833ae69339d6e0d4e5ffd0
matheusjuda <matheusbj582@gmail.com>
2024-03-25 09:03:07 -0300

    Projeto finalizado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,20 +2,20 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
-  const placeholderModificada = `${props.placeholder}...`
+    const placeholderModificada = `${props.placeholder}...`
 
-  const aoDigitado = (evento) => {
-    props.aoAlterado(evento.target.value)
-  }
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
 
-  return (
-    <div className="campo-texto">
-      <label>
-        {props.label}
-      </label>
-      <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada} />
-    </div>
-  )
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada} />
+        </div>
+    )
 }
 
 export default CampoTexto

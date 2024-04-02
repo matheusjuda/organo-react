ea035e7d028699e6134c50815f2a6f02d8c14296
matheusjuda <matheusbj582@gmail.com>
2024-04-02 14:48:09 -0300

    Capturando e guardando input

--- src/componentes/CampopTexto/index.js
+++ src/componentes/CampopTexto/index.js
@@ -4,11 +4,20 @@ const CampoTexto = (props) => {
 
     const placeholderModificado = `${props.placeholder}...`
 
+    //guardando o input
+    let valor = 'fdfg'
+
+    //capturando input
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className='campo-texto'>
             <label>{props.label}</label>
-            {/*required= obrigatorio para validar formulario*/}
-            <input required={props.obrigatorio} placeholder={placeholderModificado} />
+            {/*required= obrigatorio para validar formulario / onChange= evento ocorre quando o valor de um elemento HTML é alterado*/}
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificado} />
         </div>
     )
 }

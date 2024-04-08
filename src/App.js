489787d3eb2ec16a083625db42cf1e25ad008818
matheusjuda <matheusbj582@gmail.com>
2024-04-08 11:41:35 -0300

    criando lista de times e cores / percorrendo a lista com map

--- src/App.js
+++ src/App.js
@@ -5,6 +5,47 @@ import Time from "./componentes/Time";
 
 function App() {
 
+  //Lista de times e cores
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9',
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF',
+    },
+    {
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5',
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+    }
+
+
+  ]
+
   //cada vez q o colaborador for cadastrado acrescente a lista
   const [colaboradores, setColaboradores] = useState([])
 
@@ -18,9 +59,9 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario colaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
-      <Time nome='Programação' />
-      <Time nome='Front-End' />
-      <Time nome='Mobile' />
+      {/*Para cada time da lista de times cria-se um componente chamado Time com esse nome*/}
+      {times.map(time => <Time key={time.nome} nome={time.nome} />)}
+
 
     </div>
   );
